a2e212cec1027b30753d0596d814fa9e95781995
mehmetA58 <mehmet.akbayir58@gmail.com>
2021-09-24 22:34:16 +0300

    24/9

--- ders3_fonksiyonlar/app.js
+++ ders3_fonksiyonlar/app.js
@@ -14,9 +14,23 @@ function tekCift(sayiniz) {
     return sayiniz % 2 == 0 ? `${sayimiz}çifttir` : `${sayimiz} tektir`;
 }
 //!2.Yöntem:Function Expression
-
+//örnek1
 const tekCift1 = function(sayi) {
     return sayi % 2 == 0 ? "CİFT" : "TEK";
 };
 
 console.log(tekCift1(5));
+
+// ornek2 verilen 3 sayidan en büyük sayıyı bulma
+let buyukBul = function(a, b, c) {
+    let enBuyuk;
+    if (a > b && a > c) {
+        enBuyuk = a;
+    } else if (b > c && b > a) {
+        enBuyuk = b;
+    } else {
+        enBuyuk = c;
+    }
+    return enBuyuk;
+};
+console.log(buyukBul(5, 8, 9));
