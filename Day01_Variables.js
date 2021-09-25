38dd840abcbff0e2e85908bf7b1777ee9cb85b51
MehmetAKBAYIR <87230646+mehmetA58@users.noreply.github.com>
2021-09-25 12:29:51 +0300

    Create Day01_Variables.js

---
+++ Day01_Variables.js
@@ -0,0 +1,26 @@
+var myName;
+console.log(myName);//Varsayılan olarak, değişkenler tanımsızdır
+//var değişkenleri birden çok kez kullanılabilir
+
+var myName='Javascript';
+console.log(myName);//"Javascript"
+
+myName='Java';
+console.log(myName);//"Java"
+
+myName=true;
+console.log(myName)//true
+
+myName=6;
+console.log(myName);//6
+
+//varfName
+//var lName
+var fName='John',lName="Cash";
+console.log(fName);//"John"
+console.log(lName);//"Cash"
+
+var firstName='Sam'
+var lastName='Walton'
+
+console.log(firstName+' '+lastName);//"Sam Walton"
