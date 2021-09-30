b2bb078beff9921183c572c25f218509aef9aa8f
mehmetA58 <mehmet.akbayir58@gmail.com>
2021-09-30 22:12:33 +0300

    .

--- ders5_diziler/ornek3_iterasyon/iterasyon.js
+++ ders5_diziler/ornek3_iterasyon/iterasyon.js
@@ -1,11 +1,61 @@
-//***DİZİLER İTERASYON***** */
+// SORU: Bir hayvanat bahçesinde bulunan hayvan türlerinin
+// bir dizide saklandığı varsayalım.Bu hayvanları türüne
+// göre aramamızı sağlayacak ve o türden kaç adet bulunduğunu
+//  ana programa döndürecek fonksiyonu yaziniz. Eğer o
+// türden bir hayvan yok ise bulunamadığını yazdırsın.
 
-//***FOR DÖNGÜSÜ***** */
+const hayvanlar = [
+    "aslan",
+    "fil",
+    "kartal",
+    "kanarya",
+    "aslan",
+    "aslan",
+    "timsah",
+];
+const ara = prompt("sayısını merak ettiğin hayvanın adını yaz");
+const bulDondur = function(ara) {
+    let hayvanSayısı = 0;
+    for (let i in hayvanlar) {
+        if (hayvanlar[i] == ara) hayvanSayısı++;
+    }
+    hayvanSayısı == 0 ?
+        alert("aradıgınız hayvandan bulunamadı") :
+        alert(`aradıgınız ${ara} hayvanından ${hayvanSayısı} tane bulunmaktadır`);
+};
+bulDondur(ara);
 
-const koordinatlar = [120, 100, -100, 220, 330, -50];​
-let pozitif = 0;
-let negatif = 0;​
-for (let i = 0; i < koordinatlar.length; i++) {​​​
-    koordinatlar[i] < 0 ? negatif = negatif + koordinatlar[i] : pozitif = pozitif + koordinatlar[i];
+//*******************FOR OF DONGUSU******************** */
+// for of dongusu, for in dongusunun bir cok veri yapisini kapsayacak sekilde guncellenmis halidir.
+//Bu dongude dizi icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
+
+let arabalar = ["BMW", "Volvo", "Mini"];
+for (let i of arabalar) {
+    console.log(i);
 }
-console.log(`negatifler toplamı: ${negatif} pozifler toplamı: ${pozitif}`);
+//****FOREACH METODU************************* */
+//! foreach metodu orjinal diziyi bozmaz
+let ogrenciler = ["Onur", "Hakan", "Furkan"];
+ogrenciler.forEach((x) => console.log(x));
+
+//ornek:bütün elemanların 3 katını alınız
+
+const rakamlar = [3, 5, 3, 2, 6, 7, 9];
+console.log(rakamlar.map((x) => x * 3));
+console.log(
+    rakamlar.map((x, index) => `${index + 1}.elemanin 3 katı ${x * 3}`)
+);
+const isimler = [
+    "Ahmet",
+    "mehmet",
+    "ismet",
+    "SAFFET",
+    "Can",
+    "Canan",
+    "Cavidan",
+];
+
+const buyuk = isimler.map((isim) => isim.toUpperCase());
+console.log(buyuk);
+
+document.querySelector(".baslık").textContent = buyuk.join("->");
